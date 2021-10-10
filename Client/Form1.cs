cc795b7a310b946c15694910faea42d417178140
droivandam <84682524+droivandam@users.noreply.github.com>
2021-10-10 18:48:12 +0300

    first commit

---
+++ Client/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+    }
+}
